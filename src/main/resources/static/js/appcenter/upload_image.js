6e7f551caf87d0fa141b766ced8f64f39d474d61
hiiihiii <15708407552@163.com>
2018-06-21 20:25:32 +0800

    上传镜像页面优化

--- src/main/resources/static/js/appcenter/upload_image.js
+++ src/main/resources/static/js/appcenter/upload_image.js
@@ -13,14 +13,55 @@ define([
                     "DBMS": "数据库",
                     "Application": "应用"
                 },
-
-            },
-            mounted: function () {
+                infoTag: "baseInfo",
+                nextStep: true,
+                previousStep: false,
+                submitTag: false
 
             },
             methods: {
-
+                toNext: function () {
+                    debugger
+                    var _self = this;
+                    if(_self.infoTag == 'baseInfo'){
+                        _self.infoTag = 'configInfo';
+                        _self.previousStep = true;
+                        _self.nextStep = true;
+                    } else if(_self.infoTag == 'configInfo'){
+                        _self.infoTag = 'runtimeInfo';
+                        _self.previousStep = true;
+                        _self.nextStep = false;
+                    }
+                    if(_self.infoTag == 'runtimeInfo'){
+                        _self.submitTag = true;
+                    } else {
+                        _self.submitTag = false;
+                    }
+                },
+                toPrevious: function () {
+                    var _self = this;
+                    if(_self.infoTag == 'configInfo'){
+                        _self.infoTag = 'baseInfo';
+                        _self.previousStep = false;
+                        _self.nextStep = true;
+                    } else if(_self.infoTag == 'runtimeInfo'){
+                        _self.infoTag = 'configInfo';
+                        _self.previousStep = true;
+                        _self.nextStep = true;
+                    }
+                    if(_self.infoTag == 'runtimeInfo'){
+                        _self.submitTag = true;
+                    } else {
+                        _self.submitTag = false;
+                    }
+                }
             }
         });
+        $("#upload_image_dialog").on("hidden.bs.modal", function () {
+            upload_image.infoTag = "baseInfo";
+            upload_image.nextStep = true;
+            upload_image.previousStep = false;
+            upload_image.submitTag =  false;
+        })
     }
 });
