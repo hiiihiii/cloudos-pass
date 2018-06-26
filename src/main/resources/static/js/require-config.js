a73f045746c7458d2f9efb6c5648e546bc23a06d
hiiihiii <15708407552@163.com>
2018-06-26 11:31:15 +0800

    修改require-config文件

--- src/main/resources/static/js/require-config.js
+++ src/main/resources/static/js/require-config.js
@@ -10,7 +10,7 @@ require.config({
         "bootstrapSwitch": "lib/bootstrap/js/bootstrap-switch",
         "echarts":"lib/echarts/echarts.min",
         "jquery-validate": "lib/jquery-validation/jquery.validate.min",
-        "select2": "/lib/select2/js/select2.min",
+        "select2": "lib/select2/js/select2.min",
         "common-module":"js/common-module",
         "adminhomepage":"js/adminhomepage/adminhomepage",
         "appstore":"js/appcenter/appstore",
