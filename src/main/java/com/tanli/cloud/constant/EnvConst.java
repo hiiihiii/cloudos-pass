86a8b8f7e5dc2df2be41275b5cb119d774a6604e
tanli <15708407552@163.com>
2019-01-24 17:59:57 +0800

    修改docker配置

--- src/main/java/com/tanli/cloud/constant/EnvConst.java
+++ src/main/java/com/tanli/cloud/constant/EnvConst.java
@@ -7,7 +7,7 @@ public class EnvConst {
     /**
      * ftp服务器配置
      */
-    public static final String FTP_IP = "132.232.140.33";
+    public static final String FTP_IP = "132.232.93.3";
     public static final int FTP_PORT = 21;
     public static final String FTP_USERNAME = "docker";
     public static final String FTP_PASSWORD = "dockerfile";
@@ -17,9 +17,9 @@ public class EnvConst {
      * docker remote api配置
      */
     public static final String docker_api_protocol = "http";
-    public static final String docker_api_ip = "132.232.140.33";
+    public static final String docker_api_ip = "132.232.93.3";
     public static final String docker_api_port = "2375";
-    public static final String docker_images_prefix = "http://132.232.140.33:2375/images/";
+    public static final String docker_images_prefix = "http://132.232.93.3:2375/images/";
     public static final String harbor_username = "admin";
     public static final String harbor_password = "Harbor12345";
 
